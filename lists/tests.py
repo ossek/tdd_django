e411778c9f96a9f76da339e19ad8f25b1405b76e
ossek <guy.20.sherlocken@spamgourmet.com>
2014-11-25 00:38:41 -0600

    test impl instead of constants

--- lists/tests.py
+++ lists/tests.py
@@ -2,22 +2,19 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		print(found)
-		print(found.func)
-		# simply assert that the function we resolved to == home_page from views.py/lists.views
-		self.assertEqual(found.func, home_page)
+ def test_root_url_resolves_to_home_page_view(self):
+    found = resolve('/')
+    print(found)
+    print(found.func)
+    # simply assert that the function we resolved to == home_page from views.py/lists.views
+    self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html lang="en">'))
-		self.assertIn(b'<title>To-Do Lists</title>',response.content)
-		self.assertTrue(response.content.strip()/endswith(b'</html>'))
-
-
-
+ def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    expected_html = render_to_string('home.html')
+    self.assertEqual(response.content.decode(),expected_html)
