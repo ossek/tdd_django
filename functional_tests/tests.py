83296c93e5649b534a0b71f3c0b9a7a638cbbede
ossek <guy.20.sherlocken@spamgourmet.com>
2014-11-28 23:12:46 -0600

    ch6 func test modify for unique urls for users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,9 +4,14 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(LiveServerTestCase):
 
+    @staticmethod
+    def getBrowser():
+        browser = webdriver.Firefox()
+        # browser = webdriver.PhantomJS()
+        return browser
+
     def setUp(self):
-        self.browser = webdriver.Firefox()
-        # self.browser = webdriver.PhantomJS()
+        self.browser = NewVisitorTest.getBrowser()
         # have selenium wait a few extra seconds for page load just in case
         self.browser.implicitly_wait(3)
 
@@ -47,25 +52,41 @@ class NewVisitorTest(LiveServerTestCase):
         #when she hits enter the page updates and now the page lists 'buy peacock feathers' in
         #a todo list
         inputbox.send_keys(Keys.ENTER)
-
         self.browser.implicitly_wait(1)
+        edith_list_url = self.browser.current_url;
+        self.assertRegex(edith_list_url,'/lists/.+');
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+        #still a text box inviting another todo add (she adds ' build a hat' )
         todo_item_2 = 'use feathers to build hat'
         # NAB -- apparently have to reget the input box
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('use feathers to build hat')
         inputbox.send_keys(Keys.ENTER)
-        # self.check_for_row_in_list_table('2: %s' % todo_item_2)
         self.browser.implicitly_wait(1)
-        self.check_for_row_in_list_table('2: use feathers to build hat')
-
-        #still a text box inviting another todo add (she adds ' make a fly' )
-        self.fail('finish the test')
-
         #page updates again and shows both items
-
+        self.check_for_row_in_list_table('2: use feathers to build hat')
         #E sees that the site has made a unique url for her list and some explanatory text
-        #to that effect
-
+        #  to that effect
         #she vists the url and the list is still there
+
+        # a new user, Francis, comes to the site
+        ## Use a new browser session to try so that no cookies or private data of edith's are left
+        self.browser.quit()
+        self.browser = NewVisitorTest.getBrowser()
+        self.browser.get(self.live_server_url)
+
+        # francis adds a todo
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Eat radishes')
+        inputbox.send_keys(Keys.ENTER)
+        # francis gets his very own url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+        # no trace of edith's list
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertIn('Eat radishes',page_text)
+
+        self.fail('finish the test')
