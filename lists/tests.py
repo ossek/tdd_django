b77cb6837bec5014cff17d0403e1b8e860e80011
ossek <guy.20.sherlocken@spamgourmet.com>
2014-11-29 15:40:12 -0600

    adding a new list view and test for it

--- lists/tests.py
+++ lists/tests.py
@@ -14,7 +14,7 @@ class ListViewTest(TestCase):
         Item.objects.create(text = new_item_text1)
         Item.objects.create(text = new_item_text2)
 
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
         ## alternatively: 
         # request = HttpRequest()
         # response = home_page(request)
@@ -23,57 +23,49 @@ class ListViewTest(TestCase):
         self.assertContains(response,new_item_text2)
 
     def test_uses_different_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response,'list.html')
 
+
+class NewListTest(TestCase):
+
+    def test_redirects_after_post(self):
+        new_item_text = 'A new list item'
+
+        response = self.client.post('/lists/new', \
+                data = {'item_text':new_item_text})
+
+        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+        ## alternatively: 
+        # self.assertEqual(response.status_code,302)
+        # self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+
+    def test_saving_a_post_request(self):
+        new_item_text = 'A new list item'
+   
+        response = self.client.post('/lists/new', \
+                data = {'item_text':new_item_text})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, new_item_text)
+   
 
 class HomePageTest(TestCase):
 
  # seems there is a convention that a test method must be started with 'test_'
     def test_root_url_resolves_to_home_page_view(self):
-         found = resolve('/')
-         print(found)
-         print(found.func)
-         # simply assert that the function we resolved to == home_page from views.py/lists.views
-         self.assertEqual(found.func, home_page)
+        found = resolve('/')
+        print(found)
+        print(found.func)
+        # simply assert that the function we resolved to == home_page from views.py/lists.views
+        self.assertEqual(found.func, home_page)
    
     def test_home_page_returns_correct_html(self):
-         request = HttpRequest()
-         response = home_page(request)
-         expected_html = render_to_string('home.html')
-         self.assertEqual(response.content.decode(),expected_html)
-   
-    def test_home_page_can_save_a_post_request(self):
-         new_item_text = 'A new list item'
-   
-         request = HttpRequest()
-         request.method = 'POST'
-         request.POST['item_text'] = new_item_text
-   
-         response = home_page(request)
-   
-         self.assertEqual(Item.objects.count(),1)
-         new_item = Item.objects.first()
-         self.assertEqual(new_item.text, new_item_text)
-   
-
-    def test_home_page_redirects_after_post(self):
-        new_item_text = 'A new list item'
-
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = new_item_text
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world')
-
-        
-    def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertEqual(Item.objects.count(),0)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(),expected_html)
 
     
 class ItemModelTest(TestCase):
